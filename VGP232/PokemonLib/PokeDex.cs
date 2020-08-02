603aa949fa34d1bb60eb34dbcb9007ce95ca010a
LennonM1932511 <lennon.marshall@gmail.com>
2020-08-02 14:52:47 -0700

    assignment2c, library and images setup

---
+++ VGP232/PokemonLib/PokeDex.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace PokemonLib
+{
+    public class PokeDex : List<Pokemon>, IPeristence, IXmlSerializable, IJsonSerializable, ICsvSerializable
+    {
+        XmlSerializer serializer;
+
+        public PokeDex()
+        {
+            serializer = new XmlSerializer(typeof(List<Pokemon>));
+        }
+
+        public bool Load(string fileName)
+        {
+            Clear();
+            bool success = false;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                Console.WriteLine("No input file specified.");
+            }
+            else if (!File.Exists(fileName))
+            {
+                Console.WriteLine("The file specified does not exist.");
+            }
+            else
+            {
+                Console.WriteLine("Loading PokeDex from {0}", fileName);
+
+                string fileExt = Path.GetExtension(fileName);
+
+                // check for compatible file type and load file
+                if (fileExt.Equals(".csv", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    success = LoadCSV(fileName);
+                }
+                else if (fileExt.Equals(".xml", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    success = LoadXML(fileName);
+                }
+                else if (fileExt.Equals(".json", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    success = LoadJSON(fileName);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid file type: {0}", fileExt);
+                }
+            }
+            return success;
+        }
+
+        public bool Save(string fileName)
+        {
+            bool success = false;
+
+            Console.WriteLine("Saving PokeDex to {0}", fileName);
+
+            string fileExt = Path.GetExtension(fileName);
+
+            // check for compatible file type and save file
+            if (fileExt.Equals(".csv", StringComparison.InvariantCultureIgnoreCase))
+            {
+                success = SaveAsCSV(fileName);
+            }
+            else if (fileExt.Equals(".xml", StringComparison.InvariantCultureIgnoreCase))
+            {
+                success = SaveAsXML(fileName);
+            }
+            else if (fileExt.Equals(".json", StringComparison.InvariantCultureIgnoreCase))
+            {
+                success = SaveAsJSON(fileName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid file type: {0}", fileExt);
+            }
+
+            return success;
+        }
+
+        public Pokemon GetHighestDefense()
+        {
+            this.Sort(Pokemon.CompareByPokemonDefense);
+
+            return this[index: Count - 1];
+        }
+
+        public Pokemon GetHighestAttack()
+        {
+            this.Sort(Pokemon.CompareByPokemonAttack);
+
+            return this[Count - 1];
+        }
+
+        public Pokemon GetHighestHP()
+        {
+            this.Sort(Pokemon.CompareByPokemonHP);
+
+            return this[Count - 1];
+        }
+
+        public Pokemon GetHighestSpeed()
+        {
+            this.Sort(Pokemon.CompareByPokemonSpeed);
+
+            return this[Count - 1];
+        }
+
+        public Pokemon GetHighestTotal()
+        {
+            this.Sort(Pokemon.CompareByPokemonTotal);
+
+            return this[Count - 1];
+        }
+
+        public Pokemon GetHighestSpecialAttack()
+        {
+            this.Sort(Pokemon.CompareBySpecialAttack);
+
+            return this[Count - 1];
+        }
+
+        public Pokemon GetHighestSpecialDefense()
+        {
+            this.Sort(Pokemon.CompareBySpecialDefense);
+
+            return this[Count - 1];
+        }
+
+        List<Pokemon> GetAllPokemonOfType(Pokemon.PokemonType type)
+        {
+            // create list to save pokemon of same type
+            PokeDex pokemons = new PokeDex();
+
+            // check each pokemon for the matching type
+            foreach (Pokemon pokemon in this)
+            {
+                if (pokemon.Type1 == type || pokemon.Type2 == type)
+                {
+                    pokemons.Add(pokemon);
+                }
+            }
+
+            // return a PokeDex containing only pokemon of matching type
+            return pokemons;
+        }
+
+        public void SortBy(string columnName)
+        {
+            // determine the column name to trigger a different sort.
+            if (string.IsNullOrEmpty(columnName) || columnName.Equals("index", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Sorts the list based off of the Pokemon index.                    
+                Sort(Pokemon.CompareByIndex);
+            }
+            else if (columnName.Equals("name", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Sorts the list based off of the Pokemon name.
+                Sort(Pokemon.CompareByPokemonName);
+            }
+            else if (columnName.Equals("hp", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Sorts the list based off of the Pokemon HP.
+                Sort(Pokemon.CompareByPokemonHP);
+            }
+            else if (columnName.Equals("attack", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Sorts the list based off of the Pokemon Attack.
+                Sort(Pokemon.CompareByPokemonAttack);
+            }
+            else if (columnName.Equals("defense", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Sorts the list based off of the Pokemon Defense.
+                Sort(Pokemon.CompareByPokemonDefense);
+            }
+            else if (columnName.Equals("specialattack", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Sorts the list based off of the Pokemon SpecialAttack.
+                Sort(Pokemon.CompareBySpecialAttack);
+            }
+            else if (columnName.Equals("specialdefense", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Sorts the list based off of the Pokemon SpecialDefense.
+                Sort(Pokemon.CompareBySpecialDefense);
+            }
+            else if (columnName.Equals("speed", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Sorts the list based off of the Pokemon Speed.
+                Sort(Pokemon.CompareByPokemonSpeed);
+            }
+            else if (columnName.Equals("total", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Sorts the list based off of the Pokemon Total.
+                Sort(Pokemon.CompareByPokemonTotal);
+            }
+            else
+            {
+                Console.WriteLine("The column name, [{0}] is invalid", columnName);
+            }
+        }
+
+        public bool LoadXML(string path)
+        {
+            Clear();
+            bool success = false;
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    List<Pokemon> temp = serializer.Deserialize(reader) as List<Pokemon>;
+                    if (temp != null)
+                    {
+                        this.Clear();
+                        this.AddRange(temp);
+                    }
+                    success = true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occurred attempting to deserialize from XML");
+            }
+            return success;
+        }
+
+        public bool SaveAsXML(string path)
+        {
+            bool success = false;
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    serializer.Serialize(writer, this);
+                    success = true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occurred attempting to serialize to XML");
+            }
+            return success;
+        }
+
+        public bool LoadJSON(string path)
+        {
+            Clear();
+            bool success = false;
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string jsonData = reader.ReadToEnd();
+                    PokeDex temp = JsonConvert.DeserializeObject<PokeDex>(jsonData);
+                    if (temp != null)
+                    {
+                        this.Clear();
+                        this.AddRange(temp);
+                    }
+                    success = true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occurred attempting to access {0}", path.ToString());
+            }
+            return success;
+        }
+
+        public bool SaveAsJSON(string path)
+        {
+            bool success = false;
+            try
+            {
+                string jsonData = JsonConvert.SerializeObject(this);
+
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.Write(jsonData);
+                    success = true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occurred attempting to access {0}", path.ToString());
+            }
+            return success;
+        }
+
+        public bool LoadCSV(string path)
+        {
+            Clear();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                // Skip the first line because header does not need to be parsed.                
+                string header = reader.ReadLine();
+
+                // Parse each line and add to list
+                while (reader.Peek() > 0)
+                {
+                    if (Pokemon.TryParse(reader.ReadLine(), out Pokemon pokemon))
+                    {
+                        this.Add(pokemon);
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool SaveAsCSV(string path)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine("Nat,Pokemon,HP,Atk,Def,SpA,SpD,Spe,Total,TypeI,TypeII");
+
+                    // write data
+                    for (int i = 0; i < Count; i++)
+                    {
+                        writer.WriteLine(this[i]); // write line
+                    }
+
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occurred attempting to access {0}", path.ToString());
+                return false;
+            }
+        }
+    }
+}
---
+++ VGP232/PokemonLib/Pokemon.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLib
+{
+    [Serializable]
+    public class Pokemon
+    {
+        public enum PokemonType
+        {
+            None, Grass, Fire, Water, Bug, Normal, Poison, Electric, Ground, Fairy,
+            Fighting, Psychic, Rock, Ghost, Ice, Dragon, Flying, Dark, Steel
+        }
+
+        // Nat,Pokemon,HP,Atk,Def,SpA,SpD,Spe,Total,Type1,Type2
+        public string Index { get; set; }
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpecialAttack { get; set; }
+        public int SpecialDefense { get; set; }
+        public int Speed { get; set; }
+        public int Total { get; set; }
+        public PokemonType Type1 { get; set; }
+        public PokemonType Type2 { get; set; }
+
+        public Pokemon()
+        {
+        }
+
+        public static bool TryParse(string line, out Pokemon pokemon)
+        {
+            bool success = false;
+            pokemon = null;
+            string[] values = line.Split(',');
+
+            try
+            {
+                Pokemon temp = new Pokemon();
+
+                // test and set pokemon variables
+                temp.Index = values[0];
+                temp.Name = values[1];
+                int.TryParse(values[2], out int hp);
+                temp.HP = hp;
+                int.TryParse(values[3], out int atk);
+                temp.Attack = atk;
+                int.TryParse(values[4], out int def);
+                temp.Defense = def;
+                int.TryParse(values[5], out int spa);
+                temp.SpecialAttack = spa;
+                int.TryParse(values[6], out int spd);
+                temp.SpecialDefense = spd;
+                int.TryParse(values[7], out int spe);
+                temp.Speed = spe;
+                int.TryParse(values[8], out int total);
+                temp.Total = total;
+
+                // check if TypeI string matches a PokemonType
+                Enum.TryParse(values[9], out PokemonType type1);
+                temp.Type1 = type1;
+
+                // check if TypeII string matches a PokemonType
+                Enum.TryParse(values[10], out PokemonType type2);
+                temp.Type2 = type2;
+
+                pokemon = temp;
+                success = true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Parsing Data Failed");
+            }
+
+            return success;
+        }
+
+        /// <summary>
+        /// The Comparator function to check for name
+        /// </summary>
+        /// <param name="left">Left side Pokemon</param>
+        /// <param name="right">Right side Pokemon</param>
+        /// <returns> -1 (or any other negative value) for "less than", 0 for "equals", or 1 (or any other positive value) for "greater than"</returns>        
+        public static int CompareByPokemonName(Pokemon left, Pokemon right)
+        {
+            return left.Name.CompareTo(right.Name);
+        }
+
+        public static int CompareByIndex(Pokemon left, Pokemon right)
+        {
+            return left.Index.CompareTo(right.Index);
+        }
+
+        public static int CompareByPokemonHP(Pokemon left, Pokemon right)
+        {
+            return left.HP.CompareTo(right.HP);
+        }
+
+        public static int CompareByPokemonAttack(Pokemon left, Pokemon right)
+        {
+            return left.Attack.CompareTo(right.Attack);
+        }
+
+        public static int CompareByPokemonDefense(Pokemon left, Pokemon right)
+        {
+            return left.Defense.CompareTo(right.Defense);
+        }
+
+        public static int CompareBySpecialAttack(Pokemon left, Pokemon right)
+        {
+            return left.SpecialAttack.CompareTo(right.SpecialAttack);
+        }
+
+        public static int CompareBySpecialDefense(Pokemon left, Pokemon right)
+        {
+            return left.SpecialDefense.CompareTo(right.SpecialDefense);
+        }
+
+        public static int CompareByPokemonSpeed(Pokemon left, Pokemon right)
+        {
+            return left.Speed.CompareTo(right.Speed);
+        }
+
+        public static int CompareByPokemonTotal(Pokemon left, Pokemon right)
+        {
+            return left.Total.CompareTo(right.Total);
+        }
+
+        public override string ToString()
+        {
+            // construct a string to return with the following format
+            // Nat,Pokemon,HP,Atk,Def,SpA,SpD,Spe,Total
+            return string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}", Index, Name, HP, Attack, Defense,
+                SpecialAttack, SpecialDefense, Speed, Total, Type1.ToString(), Type2 == PokemonType.None ? "" : Type2.ToString());
+        }
+    }
+}

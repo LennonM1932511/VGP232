57d1096d5d42534093cd28a6041ae27927a05642
LennonM1932511 <lennon.marshall@gmail.com>
2020-08-28 14:06:40 -0700

    Added WeaponLib

---
+++ VGP232/WeaponLib/Weapon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponLib
+{
+    [Serializable]
+    public class Weapon
+    {
+        public enum Category
+        {
+            Simple_Melee, Simple_Ranged, Martial_Melee, Martial_Ranged
+        }
+
+        public enum DamageType
+        {
+            Bludgeoning, Piercing, Slashing, Acid, Cold, Fire,
+            Force, Lightning, Necrotic, Poison, Psychic, Radiant, Thunder
+        }
+                
+        public string Name { get; set; }
+        public Category category { get; set; }
+        public string Price { get; set; } // format: gold, decimal
+        public string Weight { get; set; } // format: pounds, decimal
+        public string Damage { get; set; } // format: XdY
+        public DamageType damageType { get; set; }
+        public string Range { get; set; } // format: X, or X/Y
+        public string Boosts { get; set; } // format: comma separated
+        public string Properties { get; set; } // format: comma separated
+        public string Description { get; set; }
+        public string Image { get; set; } // path to optional image
+        
+
+        public Weapon()
+        {
+        }        
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}, {2} gp, {3} lbs, {4} {5}, {6} ft\n{7}, {8}\n{9}\n{10}\n", 
+                category.ToString(), Name, Price, Weight, Damage, damageType.ToString(), Range, 
+                Boosts, Properties, 
+                Description, 
+                Image);
+        }
+    }
+}
---
+++ VGP232/WeaponLib/WeaponList.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace WeaponLib
+{
+    public class WeaponList : List<Weapon>, IPeristence, IXmlSerializable, IJsonSerializable
+    {
+        XmlSerializer serializer;
+
+        public WeaponList()
+        {
+            serializer = new XmlSerializer(typeof(List<Weapon>));
+        }
+
+        public bool Load(string fileName)
+        {
+            Clear();
+            bool success = false;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                Console.WriteLine("No input file specified.");
+            }
+            else if (!File.Exists(fileName))
+            {
+                Console.WriteLine("The file specified does not exist.");
+            }
+            else
+            {
+                Console.WriteLine("Loading WeaponList from {0}", fileName);
+
+                string fileExt = Path.GetExtension(fileName);
+
+                // check for compatible file type and load file                
+                if (fileExt.Equals(".xml", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    success = LoadXML(fileName);
+                }
+                else if (fileExt.Equals(".json", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    success = LoadJSON(fileName);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid file type: {0}", fileExt);
+                }
+            }
+            return success;
+        }
+
+        public bool Save(string fileName)
+        {
+            bool success = false;
+
+            Console.WriteLine("Saving WeaponList to {0}", fileName);
+
+            string fileExt = Path.GetExtension(fileName);
+
+            // check for compatible file type and save file            
+            if (fileExt.Equals(".xml", StringComparison.InvariantCultureIgnoreCase))
+            {
+                success = SaveAsXML(fileName);
+            }
+            else if (fileExt.Equals(".json", StringComparison.InvariantCultureIgnoreCase))
+            {
+                success = SaveAsJSON(fileName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid file type: {0}", fileExt);
+            }
+
+            return success;
+        }       
+
+        public List<Weapon> GetWeaponCategory(Weapon.Category category)
+        {
+            // create list to save weapons of same category
+            WeaponList weapons = new WeaponList();
+
+            // check each weapon for the matching category
+            foreach (Weapon weapon in this)
+            {
+                if (weapon.category == category)
+                {
+                    weapons.Add(weapon);
+                }
+            }
+
+            // return a WeaponList containing only weapons of matching category
+            return weapons;
+        }        
+
+        public bool LoadXML(string path)
+        {
+            Clear();
+            bool success = false;
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    List<Weapon> temp = serializer.Deserialize(reader) as List<Weapon>;
+                    if (temp != null)
+                    {
+                        this.Clear();
+                        this.AddRange(temp);
+                    }
+                    success = true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occurred attempting to deserialize from XML");
+            }
+            return success;
+        }
+
+        public bool SaveAsXML(string path)
+        {
+            bool success = false;
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    serializer.Serialize(writer, this);
+                    success = true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occurred attempting to serialize to XML");
+            }
+            return success;
+        }
+
+        public bool LoadJSON(string path)
+        {
+            Clear();
+            bool success = false;
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string jsonData = reader.ReadToEnd();
+                    WeaponList temp = JsonConvert.DeserializeObject<WeaponList>(jsonData);
+                    if (temp != null)
+                    {
+                        this.Clear();
+                        this.AddRange(temp);
+                    }
+                    success = true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occurred attempting to access {0}", path.ToString());
+            }
+            return success;
+        }
+
+        public bool SaveAsJSON(string path)
+        {
+            bool success = false;
+            try
+            {
+                string jsonData = JsonConvert.SerializeObject(this);
+
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.Write(jsonData);
+                    success = true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occurred attempting to access {0}", path.ToString());
+            }
+            return success;
+        }       
+    }
+}
